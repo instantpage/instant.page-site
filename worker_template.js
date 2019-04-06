c9545c02f268d0c64e77836d5b8c206dddbb02ab
Alexandre Dieulot <adieulot@gmail.com>
2019-04-06 14:27:36 +0200

    1 hour delay between GitHub stars update

--- worker_template.js
+++ worker_template.js
@@ -77,7 +77,7 @@ let githubStars
 let githubStarsLastFetch = 0
 
 async function getGithubStars() {
-  if (+new Date - githubStarsLastFetch < 60 * 1000 * 20) {
+  if (+new Date - githubStarsLastFetch < 60 * 60 * 1000) {
     return
   }
   githubStarsLastFetch = +new Date
