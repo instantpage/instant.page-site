e85887eaecde62b30714e985341c69109e26e2e0
Alexandre Dieulot <adieulot@gmail.com>
2020-03-07 05:21:00 +0100

    Use CSS instead of JavaScript to adjust the scroll position on hash URL using scroll-margin-top

--- scripts/fix-hash-position.js
+++
@@ -1,8 +0,0 @@
-if (location.hash) {
-  requestAnimationFrame(() => {
-    const headerHeight = document.querySelector('.header').offsetHeight
-    const margin = 25 // magic number
-    const newPosition = scrollY - headerHeight - margin
-    scrollTo(0, newPosition)
-  })
-}
