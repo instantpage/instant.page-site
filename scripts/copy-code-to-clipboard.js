76c49e05df256d2c1135fc5bd99531f182c01cc8
Alexandre Dieulot <adieulot@gmail.com>
2023-02-17 21:45:46 +0100

    Refactor copy-code-to-clipboard.js
    Drops support for Safari 12.1–13.0 and require HTTPS or localhost (navigator.clipboard)

--- scripts/copy-code-to-clipboard.js
+++ scripts/copy-code-to-clipboard.js
@@ -1,27 +1,10 @@
-const copyElement = document.createElement('textarea')
-copyElement.readOnly = true
-copyElement.style.setProperty('position', 'absolute')
-copyElement.style.setProperty('left', '-9999px')
-let appended = false
+document.querySelectorAll('.js-copy-snippet-button').forEach(($copyButton) => {
+  $copyButton.addEventListener('click', (event) => {
+    const $parent = event.target.closest('.cta-widget')
+    const $code = $parent.querySelector('.main-code-snippet__code')
+    navigator.clipboard.writeText($code.innerText)
 
-function copyToClipboard(value) {
-  if (!appended) {
-    document.body.appendChild(copyElement)
-    appended = true
-  }
-  copyElement.value = value
-  copyElement.style.setProperty('top', `${scrollY}px`) // Needed on Firefox, otherwise it scrolls to the top
-  copyElement.select()
-  copyElement.setSelectionRange(0, copyElement.value.length) // Needed on iOS
-  document.execCommand('copy')
-}
-
-Array.from(document.querySelectorAll('.js-copy-snippet-button')).forEach((element) => {
-  element.addEventListener('click', () => {
-    copyToClipboard(document.querySelector('.main-code-snippet__code').innerText)
-
-    const index = Array.from(document.querySelectorAll('.js-copy-snippet-button')).indexOf(element)
-    const copiedCtaElement = document.querySelectorAll('.cta__widget__copied-cta')[index]
-    copiedCtaElement.classList.add('cta__widget__copied-cta--displayed')
+    const $copiedCta = $parent.querySelector('.cta__widget__copied-cta')
+    $copiedCta.classList.add('cta__widget__copied-cta--displayed')
   })
 })
