38e5cf1a0a9f796898461ec7007e063366dc4ea8
Alexandre Dieulot <adieulot@gmail.com>
2023-02-23 18:54:39 +0100

    Refactor: scripts/footnotes.js

--- scripts/footnotes.js
+++ scripts/footnotes.js
@@ -1,10 +1,7 @@
-const footnoteButtons = Array.from(document.querySelectorAll('.footnote-button'))
-
-footnoteButtons.forEach((element) => {
-  element.addEventListener('click', function (event) {
-    const index = footnoteButtons.indexOf(this)
-    const footnoteContent = document.querySelectorAll('.footnote-content')[index]
-    footnoteContent.classList.toggle('footnote-content--displayed')
+document.querySelectorAll('.footnote-button').forEach(($button, index) => {
+  $button.addEventListener('click', function () {
+    const $content = document.querySelectorAll('.footnote-content')[index]
+    $content.classList.toggle('footnote-content--displayed')
     this.classList.toggle('footnote-button--activated')
   })
 })
